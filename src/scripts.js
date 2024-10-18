1c65536d1547795aa9e7442764d33e19628d27d2
Robin Ryan <robin1ryan@hotmail.com>
2024-10-18 08:40:22 +0200

    ugly code. WIll continue course, but might clean this code up later on. Project is done.

--- src/scripts.js
+++ src/scripts.js
@@ -1,47 +1,81 @@
-import { API_GIPHY_KEY, API_WEATHER_KEY } from "./configData";
+import { API_WEATHER_KEY } from "./configData";
 
-// get img data from giphy
-async function loadImg(source) {
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=${API_GIPHY_KEY}&s=${source}`,
-    { mode: "cors" }
-  );
-
-  const imgData = await response.json();
-  console.log("giphy", imgData);
-}
-
-async function loadWeather(city) {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_WEATHER_KEY}
+const loadWeather = async (city) => {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_WEATHER_KEY}
 `,
-    { mode: "cors" }
-  );
-  const imgData = await response.json();
-  let celcious = parseInt((imgData.currentConditions.temp - 32) / (9 / 5));
-  let tempLowest = parseInt((imgData.days[0].tempmin - 32) / (9 / 5));
-  let tempHighest = parseInt((imgData.days[0].tempmax - 32) / (9 / 5));
-  console.log(tempLowest, tempHighest);
-  console.log(imgData);
-  console.log("weather", imgData.currentConditions.temp, "-", celcious);
-  console.log("city", imgData.address);
-  console.log("condition", imgData.currentConditions.icon);
+      { mode: "cors" }
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+    return {
+      weatherData,
+    };
+  } catch (error) {
+    console.error("error:", error);
+    return null;
+  }
+};
 
-  const cityText = skipDocu("#basicInfo h2");
-  const cityClouds = skipDocu("#basicInfo h3");
-  const cityTemp = skipDocu("#basicInfo h1");
-  const cityHL = skipDocu("#basicInfo h5");
-  changeText(cityText, imgData.address);
-  changeText(cityClouds, imgData.currentConditions.icon);
-  changeText(cityTemp, celcious);
-  changeText(cityHL, `${tempLowest}/${tempHighest}`);
-}
+const getInput = async () => {
+  let inputValue = document.querySelector("#inputDiv input").value;
+  if (!inputValue) {
+    console.log("no input");
+  } else {
+    await applyData();
+  }
+};
 
+const fetchData = async (value) => {
+  let result = await loadWeather(value);
+  if (result) {
+    let cityName = result.weatherData.address;
+
+    let weatherConds = result.weatherData.currentConditions.icon;
+
+    // calculates fahrenheit as celcious
+    let currTemp = Math.round(
+      (result.weatherData.currentConditions.temp - 32) / (9 / 5)
+    );
+
+    // calculates fahrenheit as celcious
+    let highLow = `L:${Math.round(
+      (result.weatherData.days[0].tempmin - 32) / (9 / 5)
+    )}  H:${Math.round((result.weatherData.days[0].tempmax - 32) / (9 / 5))}`;
+
+    return {
+      cityName,
+      weatherConds,
+      currTemp,
+      weatherConds,
+      highLow,
+    };
+  }
+};
+
+// update text for contentContainerOne
 const changeText = (element, text) => {
   return (element.textContent = text);
 };
 
+// shortens document.querySelector
 const skipDocu = (id) => document.querySelector(id);
 
-loadImg("hulk");
-loadWeather("Leipzig");
+const applyData = async () => {
+  const element = await fetchData(
+    document.querySelector("#inputDiv input").value
+  );
+  changeText(
+    skipDocu("#basicInfo h2"),
+    element.cityName.charAt(0).toUpperCase() + element.cityName.slice(1)
+  );
+  changeText(
+    skipDocu("#basicInfo h3"),
+    element.weatherConds.charAt(0).toUpperCase() + element.weatherConds.slice(1)
+  );
+  changeText(skipDocu("#basicInfo h1"), element.currTemp);
+  changeText(skipDocu("#basicInfo h5"), element.highLow);
+};
+
+document.querySelector("#inputDiv button").addEventListener("click", getInput);
